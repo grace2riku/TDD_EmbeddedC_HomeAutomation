b66eb7e87038c8b541904ab962b2c6875866575c
Koji Abe <gracehime@gmail.com>
2022-04-14 23:13:20 +0900

    p160 LightSchedulerRandomizeグループのテストを書くファイルLightSchedulerRandomizeTest.cppを作成した。スケルトンのテストが失敗することを確認した。

---
+++ tests/HomeAutomation/LightSchedulerRandomizeTest.cpp
@@ -0,0 +1,25 @@
+extern "C"
+{
+//#include "LightSchedulerRandomize.h"
+}
+
+#include "CppUTest/TestHarness.h"
+
+TEST_GROUP(LightSchedulerRandomize)
+{
+    void setup()
+    {
+//      LightSchedulerRandomize_Create();
+    }
+
+    void teardown()
+    {
+//       LightSchedulerRandomize_Destroy();
+    }
+};
+
+TEST(LightSchedulerRandomize, Create)
+{
+  FAIL("LightSchedulerRandomize Start here");
+}
+
--- tests/RunAllTests.cpp
+++ tests/RunAllTests.cpp
@@ -5,6 +5,7 @@ IMPORT_TEST_GROUP(LightScheduler);
 IMPORT_TEST_GROUP(LightControllerSpy);
 IMPORT_TEST_GROUP(FakeTimeService);
 IMPORT_TEST_GROUP(RandomMinute);
+IMPORT_TEST_GROUP(LightSchedulerRandomize);
 
 int main(int argc, char** argv)
 {
